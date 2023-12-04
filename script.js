74a2ef667c9ad8685cf803337c1ffc1e1e186954
Caio Vinicius <135247282+CaioViniF@users.noreply.github.com>
2023-12-04 18:50:19 -0300

    Finish script.js

--- script.js
+++ script.js
@@ -15,16 +15,18 @@ let gameRun = {
     pause: false,
 }
 
-let score = 0; // Inicializa o contador
+let score = 0; // Inicia o contador de avoões pousados
 
 function createAircraft() {
     return {
-        // variação de posições iniciais, sendo X fixa no início da tela, e Y aleatória
+        // Variação de posições iniciais, sendo X fixa no início da tela, e Y aleatória
         x: 50,
         y: Math.floor(Math.random() * (canvas.height - aviao_size)),
-        start: 1,
+        start: -10,
         direction: 1,
         visible: true,
+        name: "Avião " + (avioes.length + 1), // Adiciona um nome único para cada avião
+        gasolina: Math.floor(Math.random() * (10 - 3 + 1)) + 3, // Adiciona gasolina entre 3 e 10
     };
 }
 
@@ -36,18 +38,34 @@ function gameLoop() {
     }
 
     canvasContext.clearRect(0, 0, canvas.width, canvas.height);
-
     for (let i = 0; i < avioes.length; i++) {
         let aviao = avioes[i];
 
+        // Descrição do avião e gasolina na tela do canvas
         if (aviao.visible) {
-            canvasContext.drawImage(image, aviao.start, aviao.y, aviao_size, aviao_size)
-            aviao.start = aviao.start + (aviao.direction * gameRun.speed);
+            canvasContext.drawImage(image, aviao.start, aviao.y, aviao_size, aviao_size);
+            canvasContext.font = "12px Arial";
+            canvasContext.fillStyle = "black";
+            canvasContext.fillText(aviao.name, aviao.start, aviao.y - 5);
+            canvasContext.fillText("Gasolina: " + Math.floor(aviao.gasolina), aviao.start, aviao.y + aviao_size + 15);
 
-            if (aviao.start >= canvas.width) {
-                // Desaparece quando atinge o final da tela
+            if (aviao.gasolina <= 0) {
                 aviao.visible = false;
-                score++; // Incrementa o contador quando um avião chega ao final
+                alert(`Avião ${aviao.name} caiu por falta de gasolina!`);
+            } else if (aviao.gasolina < 3) {
+                // Aumenta a velocidade se a gasolina for menor que 3
+                aviao.start += (aviao.direction * (gameRun.speed * 1.5));
+            } else {
+                aviao.start += (aviao.direction * gameRun.speed);
+            }
+
+            // Reduz a gasolina depois de cada gameloop
+            aviao.gasolina -= 0.01;
+
+            // Pousa o avião
+            if (aviao.start >= canvas.width) {
+                aviao.visible = false;
+                score++;
                 console.log("Score: " + score);
                 cont.innerHTML = `${score} aviões pousados`;
             }
@@ -55,7 +73,6 @@ function gameLoop() {
     }
 }
 
-// Inicia o loop do jogo
 setTimeout(gameLoop, gameRun.fps);
 
 // Adiciona um novo avião ao array
@@ -63,12 +80,13 @@ function addAircraft() {
     avioes.push(createAircraft());
 }
 
-// Adiciona um novo avião a cada 3 segundos
+// Adiciona um novo avião a cada 1 segundo
 setInterval(addAircraft, 1000);
 
 document.addEventListener('keydown', function (event) {
     console.log(event.key);
 
+    // Pausa o game
     if (event.key == 'p') {
         gameRun.pause = !gameRun.pause;
     }
